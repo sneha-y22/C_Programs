6b970495af62df7dcbe98d0814226f24c9cff180
Sneha Yadav <140615190+sneha-y22@users.noreply.github.com>
2023-08-07 19:42:26 +0530

    Program_to_calculate_sum_of_two_numbers.c

--- Program_to_calculate_sum_of_two_numbers.c
+++ Program_to_calculate_sum_of_two_numbers.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
-  
-int main()
-{
-    int A, B, sum = 0;
-  
-    // Ask user to enter the two numbers
-    printf("Enter two numbers A and B : \n");
-  
-    // Read two numbers from the user || A = 2, B = 3
-    scanf("%d%d", &A, &B);
-  
-    // Calculate the addition of A and B
-    // using '+' operator
-    sum = A + B;
-  
-    // Print the sum
-    printf("Sum of A and B is: %d", sum);
-  
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculate the sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
     return 0;
 }
