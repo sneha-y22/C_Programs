6361088c388c9149e00b97399894a01eca775c36
Sneha Yadav <140615190+sneha-y22@users.noreply.github.com>
2023-08-08 21:02:53 +0530

    Create Program_to_print_Pyramids_and_Patterns

---
+++ Program_to_print_Pyramids_and_Patterns.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+// Output
+*
+* *
+* * *
+* * * *
+* * * * * 
+
+
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("%d ", j);
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+// Output
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+
+#include <stdio.h>
+int main() {
+   int i, j;
+   char input, alphabet = 'A';
+   printf("Enter an uppercase character you want to print in the last row: ");
+   scanf("%c", &input);
+   for (i = 1; i <= (input - 'A' + 1); ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("%c ", alphabet);
+      }
+      ++alphabet;
+      printf("\n");
+   }
+   return 0;
+}
+
+// Output
+A
+B B
+C C C
+D D D D
+E E E E E
